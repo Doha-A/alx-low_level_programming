4e44cae2ee9c53bd25c4af039432fbb072b9dc64
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-10 23:31:32 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - A program printing \"Programming is like building a multilingual puzzle
+ * main - A program printing \"Programming is like
+ * building a multilingual puzzle
  * Return: returning 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+puzzle");
 	return (0);
 }
