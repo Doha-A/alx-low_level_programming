85e92b9653ada86878a3d518267b1d2b0075e37e
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-10 23:22:43 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * A program printin "Programming is like building a multilingual puzzle
- * return 0
-*/
+ * main - A program printin \"Programming is like building a multilingual puzzle
+ * returning 0
+ */
 
 int main(void)
 {
