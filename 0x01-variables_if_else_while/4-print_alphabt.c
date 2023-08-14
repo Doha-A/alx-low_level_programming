d38f87ca0f6d987fc769cdfb7092731be86ebfb7
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-14 22:34:56 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphapet except the letters 'q' and 'e'
+ * Return: returning 0 success
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			break;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
