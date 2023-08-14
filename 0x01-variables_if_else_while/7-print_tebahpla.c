3d1cd36884692cdd8e46f399058c992acf10de88
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-15 02:42:01 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphapet in reverse in lowercase
+ * Return: returning 0 success
+*/
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'z'; c => 'a'; c--)
+		putchar(c);
+	putchar("\n");
+}
