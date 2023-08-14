84b97171ebc31253b458d841cde047a19623b8bc
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-14 22:53:47 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the numbers from 0 to 9
+ * Return: returning 0 success
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
