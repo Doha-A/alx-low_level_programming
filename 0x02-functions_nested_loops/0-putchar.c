9b677eb4482203119a24dfcaa13e99d989d2e766
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-15 15:15:02 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints "_putchar"
+ * Return: returning 0 success
+*/
+
+int main(void)
+{
+	char c = _putchar;
+
+	putchar(c);
+
+	return (0);
+}
