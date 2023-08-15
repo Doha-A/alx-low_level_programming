be18c4a9040152d1aa77ee52722da56a881804dd
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-15 16:19:03 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints the alphapet in lowercase
+ * Return: returning 0 success
+*/
+
+void print_alphabet(void);
+
+int main(void)
+{
+
+	putchar(print_alphabet);
+	putchar('\n');
+	return (0);
+}
