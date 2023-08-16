26c245145d6b9c9d1683a7ab3a769b61549188c9
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-16 15:08:25 +0300

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet_x10 - A program that prints the alphapet in lowercase 10 times
+ * Return: returning nothing
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
