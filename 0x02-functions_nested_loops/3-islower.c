b86a91260f91097e7c9233a17ebff47a0e058000
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-16 15:22:15 +0300

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if the letter is lowercase or uppercase
+ * Return: 1 if lowercase, 0 if uppercase
+*/
+
+int _islower(int c);
+{
+	c = c + '0';
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+}
