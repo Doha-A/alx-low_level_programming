0292998b0524d80f46d44f011569b71bada65524
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-16 12:54:59 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,2 @@
 char *Putchar = "_putchar\n";
-void print_alphabet(void)
-{
-        char c;
-
-        for (c = 'a'; c <= 'z'; c++)
-                putchar(c);
-
-        putchar('\n');
-}
-
+void print_alphabet(void);
