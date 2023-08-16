5887dd2049a7688dd2fee724acb56eb3053cc73e
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-16 16:16:11 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - a function returning the absloute value of the number
+ * @r: integre
+ * Return: returns the absloute value
+*/
+
+int _abs(int r)
+{
+	int r;
+
+	if (r < 0)
+		r = r * -1;
+	return (r);
+}
+
