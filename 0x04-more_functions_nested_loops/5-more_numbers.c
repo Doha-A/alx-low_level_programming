95b46d9d6237088171e0dbce930cb16d09878aa0
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-18 02:27:40 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers from 0 to 14 ten times.
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			++j;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
