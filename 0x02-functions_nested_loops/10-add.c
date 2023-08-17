928a7206b35f866c56d123153a9e9b4377e201ea
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-17 15:52:35 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that prints the addition of 2 numbers
+ * @num1 - first number
+ * @num2 - second number
+ * Return: returns the last digit
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
