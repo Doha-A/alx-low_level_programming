2b6dca0eb0f6b3d6746a9228e2d95a1cd108f10f
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-17 15:57:29 +0300

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints all natural numbers uo to no 98
+ * @n: first natural number
+ * Return: returns the last digit
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+}
