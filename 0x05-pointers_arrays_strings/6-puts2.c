8a330cfa60b5173d5ac1889bd49322654d8d2c32
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-22 21:10:19 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,7 +14,7 @@ void puts2(char *str)
 	count = 0;
 	while (str[count] != '\0')
 		count++;
-	for (i = 0; i <= count; i += 2)
+	for (i = 0; i < count; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
