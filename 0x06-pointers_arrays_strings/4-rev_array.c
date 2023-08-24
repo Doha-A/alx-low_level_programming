af6ecca8a2c2f90a0c339ad54d2c318cb697f086
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-24 20:08:38 +0300

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_array - a function that reverse a string.
+ * @a: int pointer
+ * @n: array length
+ * Return: returning success
+*/
+
+void reverse_array(int *a, int n);
+{
+	int i, temp;
+
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[n - i - 1];
+		s[n - i - 1] = temp;
+	}
+}
