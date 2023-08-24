bc380015dc3050fb920a69fe902c6b070a80e628
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-24 20:34:00 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,8 @@
 
 char *cap_string(char *str)
 {
+	int i, j;
+
 	char special[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
@@ -22,9 +24,7 @@ char *cap_string(char *str)
 			if (str[i] == special[j])
 			{
 				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
 					str[i + 1] -= 32;
-				}
 			}
 		}
 	}
