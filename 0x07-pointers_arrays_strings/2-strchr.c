bdafe191fb136ef60618775b4153add2bcbf745f
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-29 01:50:30 +0300

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - a function that locates a character in a string.
+ * @s: char pointer (string)
+ * @s: character looking for its first occurance
+ * Return: returns a pointer to the first occurrence of the character c
+ * returns NULL if the character is not found
+*/
+
+char *_strchr(char *s, char c)
+{
+		int a;
+
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
+}
+
