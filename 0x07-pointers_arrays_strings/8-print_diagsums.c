ea8716ec4eb5e6a1e84fa9e341a88eca4d450247
root <root@LAPTOP-6T17CF0J.localdomain>
2023-08-29 03:47:34 +0300

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - a function that prints the sum of the two diagonals of a square matrix of integers.
+ * @a: first int pointer
+ * @size: integer
+ * Return: returning nothing
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, %d\n", sum, sum1);
+}
