b74c0def5ccb2bf52b7fc3c2d80ceeba5230fc8d
root <root@LAPTOP-6T17CF0J.localdomain>
2023-09-02 00:35:07 +0300

    libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s
+ *with the constant byte b
+ * @s: first char pointer
+ * @b: second char pointer
+ * @n: unsigned int
+ * Return: returning s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
